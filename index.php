38b414d528f2d3a8555e91f48809e8ec401fb5e9
Gabriel de Paula Alvarenga Adeodato <111003605+GabrielAdeodato@users.noreply.github.com>
2023-07-06 01:19:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $conn = new mysqli("mysql", "redhat", "redhat", "workshop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$result = $conn->query("SELECT nome FROM cidade");
+$result = $conn->query("SELECT nome FROM tb_pessoas");
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<h3>" . $row["nome"] . "</h3>";

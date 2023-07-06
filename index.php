dc25031a5f9d2ddd0d1f41301d24730824757884
Gabriel de Paula Alvarenga Adeodato <111003605+GabrielAdeodato@users.noreply.github.com>
2023-07-06 01:18:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
-
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-} else {
-    printf("Connected to the database");
+echo "<h1>Openshift Workshop v2.0</h1> ";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br><hr>";
+echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
+$conn = new mysqli("mysql", "redhat", "redhat", "workshop");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-$connection->close();
+$result = $conn->query("SELECT nome FROM cidade");
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<h3>" . $row["nome"] . "</h3>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
